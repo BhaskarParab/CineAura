dc02ed3b77f030d02e36d26901d179e0be7abef7
BhaskarParab <bhaskarrparab@gmail.com>
2026-03-31 13:34:56 +0530

    prisma installed

---
+++ backend/app/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application } from "express"
+import cors from "cors"
+import router from "./routes/auth.route"
+
+const app: Application = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/auth', router)
+
+
+export default app
